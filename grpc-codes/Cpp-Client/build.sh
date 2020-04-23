ab77cf77e8aab59d88b8e9ed0f41d7ab7d52c23f
Gnani Inovations <50353862+gnani-ai@users.noreply.github.com>
2020-04-23 18:13:36 +0530

    missing -lpthread is added.

--- grpc-codes/Cpp-Client/build.sh
+++ grpc-codes/Cpp-Client/build.sh
@@ -4,7 +4,7 @@ protoc -I proto --cpp_out=. proto/stt.proto
 g++ -g -std=c++11 `pkg-config --cflags protobuf grpc`  -c -o client.o client.cc
 g++ -g -std=c++11 `pkg-config --cflags protobuf grpc`  -c -o stt.pb.o stt.pb.cc
 g++ -g -std=c++11 `pkg-config --cflags protobuf grpc`  -c -o stt.grpc.pb.o stt.grpc.pb.cc 
-g++ -g  stt.pb.o stt.grpc.pb.o client.o -L/usr/local/lib `pkg-config --libs protobuf grpc++ grpc` -Wl,--no-as-needed -lgrpc++_reflection -Wl,--as-needed -ldl -o client
+g++ -g  stt.pb.o stt.grpc.pb.o client.o -L/usr/local/lib `pkg-config --libs protobuf grpc++ grpc` -Wl,--no-as-needed -lgrpc++_reflection -Wl,--as-needed -lpthread -ldl -o client
 
    
   
